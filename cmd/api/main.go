74790eb2f3acfca5b3282ede9483480114becc3d
daulet-omarov <22791@adata.kz>
2026-03-10 16:52:20 +0500

    ATTM-2: add swagger

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,14 +1,22 @@
 package main
 
 import (
+	httpSwagger "github.com/swaggo/http-swagger"
 	"log"
 	"net/http"
 
 	"github.com/daulet-omarov/ai-task-team-manager/internal/database"
 	"github.com/daulet-omarov/ai-task-team-manager/internal/modules/auth"
 	"github.com/go-chi/chi/v5"
+
+	_ "github.com/daulet-omarov/ai-task-team-manager/docs"
 )
 
+// @title AI Task Team Manager API
+// @version 1.0
+// @description AI Task Team Manager API is a backend service designed to manage team tasks using artificial intelligence. It provides endpoints for task creation, team collaboration, role management, and AI-powered task recommendations to improve productivity and workflow management.
+// @host localhost:8080
+// @BasePath /
 func main() {
 	db := database.NewPostgres()
 
@@ -19,6 +27,8 @@ func main() {
 	r := chi.NewRouter()
 
 	auth.RegisterRoutes(r, handler)
+
+	r.Get("/swagger/*", httpSwagger.WrapHandler)
 
 	log.Println("starting server on port 8080")
 
